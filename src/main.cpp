20380e77ce6bc84ab3c11b4ec39f80a99cd53c80
Bobby <robertmurrells@gmail.com>
2021-06-08 15:42:11 +0100

    Corrected error message.

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main()
 
             if (auto err = audio_session_group.set_mute(true, NULL); !err.empty())
             {
-                std::cout << "Failed to set_master_volume for:\n";
+                std::cout << "Failed to set_mute for:\n";
                 for (auto const &[pentry, hr] : err)
                 {
                     std::cout << " pid: " << pentry.th32ProcessID << " hr: " << int_to_hex_string(hr) << '\n';
