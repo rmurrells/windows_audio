a954650df1e409c0e45029bd7840613e2a5c46ad
Bobby <robertmurrells@gmail.com>
2021-06-09 14:47:30 +0100

    Move command handling to main.

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,156 @@
 
 #include <atlexcept.h>
 
-#include "arg_map.hpp"
 #include "audio_manager.hpp"
 #include "int_to_hex_string.hpp"
+
+using Tokens = std::vector<std::string>;
+
+bool check_arg_no(Tokens const &tokens, Tokens::size_type target)
+{
+    auto actual{tokens.size()};
+    if (actual < target)
+    {
+        std::cout << "Expected " << std::to_string(target) << " arguments, got " << actual << '\n';
+        return false;
+    }
+    return true;
+}
+
+bool process(std::string const &line, AudioManager &audio_manager)
+{
+    Tokens tokens;
+    std::copy(std::istream_iterator<std::string>{std::stringstream{line}},
+              std::istream_iterator<std::string>{},
+              std::back_inserter(tokens));
+    if (tokens.empty())
+    {
+        return true;
+    }
+    auto const &command = tokens[0];
+    if (command == "help")
+    {
+        std::cout
+            << "\n"
+            << "list (l): display the names of current audio processes.\n\n"
+            << "list_info (li): display the names of current audio processes and relevant information.\n\n"
+            << "set (s) <name> <volume scale from 0 - 1>: set all processes with the process name to the supplied volume scale.\n\n"
+            << "mute (m) <name> <1 / 0>: mutes / unmute all processes with the process name.\n\n"
+            << "quit (q): terminate the program.\n\n";
+    }
+    else if (command == "list" || command == "l")
+    {
+        std::cout << "speakers\n";
+        for (auto const &audio_session_group : audio_manager.get_audio_sessions())
+        {
+            std::wcout << audio_session_group.second.get_name() << '\n';
+        }
+    }
+    else if (command == "list_info" || command == "li")
+    {
+        std::cout << "\nspeakers:\n";
+        if (auto [volume, hr] = audio_manager.get_speaker_volume(); FAILED(hr))
+        {
+            std::cout << "  Failed to get_speaker_volume with hr: " << int_to_hex_string(hr) << "\n";
+        }
+        else
+        {
+            std::cout << "  Volume: " << volume << '\n';
+        }
+        if (auto [mute, hr] = audio_manager.get_speaker_mute(); FAILED(hr))
+        {
+            std::cout << "  Failed to get_speaker_mute with hr: " << int_to_hex_string(hr) << "\n";
+        }
+        else
+        {
+            std::cout << "  Mute: " << mute << "\n\n";
+        }
+        for (auto const &audio_session_group : audio_manager.get_audio_sessions())
+        {
+            audio_session_group.second.print();
+        }
+    }
+    else if (command == "set" || command == "s")
+    {
+        if (!check_arg_no(tokens, 3))
+        {
+            return true;
+        }
+
+        auto const &vol_str = tokens[2];
+        double volume{0};
+        try
+        {
+            volume = std::stod(vol_str);
+        }
+        catch (...)
+        {
+            std::cout << "Cannot convert " << vol_str << " to volume scale.\n";
+            return true;
+        }
+
+        auto &audio_map = audio_manager.get_audio_sessions();
+        auto const &name = tokens[1];
+        if (auto it = audio_map.find(std::wstring{name.begin(), name.end()}); it != audio_map.end())
+        {
+            it->second.set_master_volume(volume, NULL);
+        }
+        else if (name == "speakers")
+        {
+            audio_manager.set_speaker_volume(volume, NULL);
+        }
+        else
+        {
+            std::cout << "Unrecognized process name: " << name << '\n';
+        }
+    }
+    else if (command == "mute" || command == "m")
+    {
+        if (!check_arg_no(tokens, 3))
+        {
+            return true;
+        }
+
+        auto const &set_str = tokens[2];
+        bool set{false};
+        try
+        {
+            set = std::stoi(set_str);
+        }
+        catch (...)
+        {
+            std::cout << "Cannot convert \"" << set_str << "\" to bool.\n";
+            return true;
+        }
+
+        auto &audio_map = audio_manager.get_audio_sessions();
+        auto const &name = tokens[1];
+        if (auto it = audio_map.find(std::wstring{name.begin(), name.end()}); it != audio_map.end())
+        {
+            it->second.set_mute(set, NULL);
+        }
+        else if (name == "speakers")
+        {
+            audio_manager.set_speaker_mute(set, NULL);
+        }
+        else
+        {
+            std::cout << "Unrecognized process name: " << name << '\n';
+        }
+    }
+    else if (command == "quit" ||  command == "q") {
+        return false;
+    }
+    else
+    {
+        std::cout << "Unrecognized command: " << line << '\n';
+    }
+    return true;
+}
 
 int main()
 {
     AudioManager audio_manager;
-    ArgMap arg_map;
     std::string line;
 
     while (true)
@@ -22,11 +164,10 @@ int main()
         std::getline(std::cin, line);
         try
         {
-            if (line == "quit")
+            if (!process(line, audio_manager))
             {
                 break;
             }
-            arg_map.process(line, audio_manager);
         }
         catch (ATL::CAtlException const &e)
         {
